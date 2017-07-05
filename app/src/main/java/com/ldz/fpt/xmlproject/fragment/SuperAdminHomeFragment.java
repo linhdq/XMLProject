88868f7c1680fd4e50f236347964ae3236b75d22
TungNT-2810 <yugioh2810@gmail.com>
2017-07-06 01:34:58 +0700

    Add remove account and call fundtion in super admin screen

--- app/src/main/java/com/ldz/fpt/xmlproject/fragment/SuperAdminHomeFragment.java
+++ app/src/main/java/com/ldz/fpt/xmlproject/fragment/SuperAdminHomeFragment.java
@@ -1,11 +1,24 @@
 package com.ldz.fpt.xmlproject.fragment;
 
+import android.app.AlertDialog;
+import android.content.ClipData;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,9 +29,11 @@ import com.ldz.fpt.xmlproject.acitivity.HomeActivity;
 import com.ldz.fpt.xmlproject.adapter.ListUserAdapter;
 import com.ldz.fpt.xmlproject.database.DBContext;
 import com.ldz.fpt.xmlproject.model.User;
+import com.ldz.fpt.xmlproject.model.XmlResponseModel;
 import com.ldz.fpt.xmlproject.network.GetService;
 import com.ldz.fpt.xmlproject.network.ServiceFactory;
 import com.ldz.fpt.xmlproject.network.model.XMLLoginSendForm;
+import com.ldz.fpt.xmlproject.network.model.XmlDeleteAccount;
 import com.ldz.fpt.xmlproject.util.Constant;
 import com.ldz.fpt.xmlproject.xml_parser.XMLParser;
 
@@ -55,11 +70,15 @@ public class SuperAdminHomeFragment extends Fragment {
     private GetService getService;
     private XMLParser xmlParser;
     private XMLLoginSendForm sendForm;
+    private XmlDeleteAccount xmlDeleteAccount;
     private User user;
     //
     private HomeActivity homeActivity;
     //
     private boolean isFirst;
+    //
+    private Paint paint;
+    private AlertDialog.Builder alertDialog;
 
     @Nullable
     @Override
@@ -67,13 +86,13 @@ public class SuperAdminHomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_super_admin_home, container, false);
         ButterKnife.bind(this, view);
         init(view);
-        configRecyclerView();
         return view;
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        configRecyclerView();
         if (user == null) {
             user = dbContext.checkLoginSuccess();
         }
@@ -95,6 +114,8 @@ public class SuperAdminHomeFragment extends Fragment {
         //
         isFirst = true;
         listUser = new ArrayList<>();
+        //
+        paint = new Paint();
     }
 
     private void configRecyclerView() {
@@ -102,6 +123,7 @@ public class SuperAdminHomeFragment extends Fragment {
         listUserAdapter = new ListUserAdapter(context, listUser);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(listUserAdapter);
+        initSwipe();
     }
 
     private void getAllAdmins(RequestBody data) {
@@ -130,4 +152,116 @@ public class SuperAdminHomeFragment extends Fragment {
             }
         });
     }
+
+    private void initSwipe() {
+        ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+
+                if (direction == ItemTouchHelper.LEFT) {
+                    initDialog(dbContext.getUserModel().getUsername(), listUser.get(position).getUsername(), position);
+                } else {
+                    callToUser(listUser.get(position).getPhoneNumber());
+                }
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                Bitmap icon;
+
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+                    View itemView = viewHolder.itemView;
+                    float height = (float) itemView.getBottom() - (float) itemView.getTop();
+                    float width = height / 3;
+
+                    if (dX > 0) {
+                        paint.setColor(Color.parseColor("#388E3C"));
+                        RectF background = new RectF((float) itemView.getLeft(), (float) itemView.getTop(), dX, (float) itemView.getBottom());
+                        c.drawRect(background, paint);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_phone_white_48dp);
+                        RectF icon_dest = new RectF((float) itemView.getLeft() + width, (float) itemView.getTop() + width, (float) itemView.getLeft() + 2 * width, (float) itemView.getBottom() - width);
+                        c.drawBitmap(icon, null, icon_dest, paint);
+                    } else {
+                        paint.setColor(Color.parseColor("#FF0000"));
+                        RectF backGround = new RectF((float) itemView.getRight() + dX, (float) itemView.getTop(), (float) itemView.getRight(), (float) itemView.getBottom());
+                        c.drawRect(backGround, paint);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_delete_white_48dp);
+                        RectF icon_dest = new RectF((float) itemView.getRight() - 2 * width, (float) itemView.getTop() + width, (float) itemView.getRight() - width, (float) itemView.getBottom() - width);
+                        c.drawBitmap(icon, null, icon_dest, paint);
+                    }
+                }
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+    }
+
+    private void callToUser(String phoneNumber) {
+        Uri call = Uri.parse("tel:" + phoneNumber);
+        Intent callIntent = new Intent(Intent.ACTION_CALL, call);
+        startActivity(callIntent);
+    }
+
+    private void reloadData(XmlResponseModel xmlResponseModel, int position) {
+        if (xmlResponseModel.getStatus()) {
+            listUserAdapter.removeItem(position);
+        }
+        homeActivity.showToast(xmlResponseModel.getMessage());
+    }
+
+    private void deleteAccountService(RequestBody requestBody, final int position) {
+        Call<ResponseBody> call = getService.callDeleteAccountAdmin(requestBody);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.code() == Constant.OK_STATUS) {
+                    try {
+                        String xml = response.body().string();
+                        XmlResponseModel xmlResponseModel = xmlParser.xmlResponse(xml);
+                        reloadData(xmlResponseModel, position);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    homeActivity.showToast("Something went wrong! Please try again later.");
+                    configRecyclerView();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                homeActivity.showToast("Login failed! Please check your connection.");
+            }
+        });
+    }
+
+    private void initDialog(final String userNameRequest, final String userNameDelete, final int position) {
+        alertDialog = new AlertDialog.Builder(context);
+        alertDialog.setTitle("Delete Account")
+                .setMessage("Are you sure you want to delete this account?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        xmlDeleteAccount = new XmlDeleteAccount(userNameRequest, userNameDelete);
+                        deleteAccountService(xmlDeleteAccount.getRequestBody(), position);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        configRecyclerView();
+                    }
+                })
+                .show();
+    }
+
 }
